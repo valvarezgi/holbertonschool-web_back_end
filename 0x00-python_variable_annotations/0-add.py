03621e03b047290267709595aaa4e21429746bca
Valeria Alvarez G <vale.agiraldo@gmail.com>
2022-01-31 22:10:29 +0000

    "README"

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""add function """
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers, a and b
+    Args:
+        a (float): number
+        b (float): number
+    Returns:
+        float: a + b
+    """
+    return a + b
---
+++ README.md
@@ -0,0 +1,3 @@
+# holbertonschool-web_back_end
+ Specializations - Web Stack programming ― Back-end
+
