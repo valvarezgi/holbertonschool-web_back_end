69ed48e21f61681a440ca53de3385f70dae3d425
Valeria Alvarez G <vale.agiraldo@gmail.com>
2022-01-31 22:18:18 +0000

    Task 2: Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""floor functions
+"""
+import math
+
+def floor(n: float) -> int:
+    """Return floor of n float number
+    Args:
+        n (float): number
+    Returns:
+        int: int(n)
+    """
+    return math.floor(n)
